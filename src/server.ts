5a403c7c119e15e1ab57b809d4b8f622b5c41c4c
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-01-07 17:44:44 +0200

    docs server & install packages

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
