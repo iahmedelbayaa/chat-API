5c3b809c35ad0ea3b69f92ecf77c847ebea70b16
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-01-07 17:56:53 +0200

    docs server

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
