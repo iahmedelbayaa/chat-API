836203ad40392aaddafaa8fd5bdf51afad6471c7
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-01-08 07:20:59 +0200

    docs database connection

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -1,3 +1,6 @@
 import mongoose from 'mongoose';
-const uri = process.env.MONGO_URI as string; ;
-mongoose.connect(uri);
+const uri = process.env.MONGO_URI as string;
+mongoose
+  .connect(uri)
+  .then(() => console.log('Mongo Connection Established'))
+  .catch((error) => console.log(' Mongo Connection Failed', error.message));
