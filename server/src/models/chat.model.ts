04fd7cdbfb701b5e92d214c26f9ea8b6d34dee45
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-01-11 01:03:22 +0200

    docs chat model& interface

---
+++ server/src/models/chat.model.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const chatSchema = new mongoose.Schema(
+  {
+    members:Array
+  },
+  { timestamps: true }
+);
+const Chat = mongoose.model('Chat', chatSchema);
+export default Chat;
