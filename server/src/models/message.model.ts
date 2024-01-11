7d35e83e819af4cab855b545b5cdf8bea5caeeba
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-01-11 02:27:51 +0200

    docs message model

---
+++ server/src/models/message.model.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema(
+  {
+    chatId: String,
+    senderId: String,
+    text: String,
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model('Message', messageSchema);
+module.exports = Message;
