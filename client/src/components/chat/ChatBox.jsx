4950d47a4bb3dc078cf416623aa334a902d2134a
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-01-13 05:14:45 +0200

    docs chat box UI

--- client/src/components/chat/ChatBox.jsx
+++ client/src/components/chat/ChatBox.jsx
@@ -1,6 +1,7 @@
 // ChatBox.jsx
 import moment from 'moment';
-import { useContext, useEffect } from 'react';
+import InputEmoji from 'react-input-emoji';
+import { useContext, useEffect, useState } from 'react';
 import { Stack } from 'react-bootstrap';
 import { ChatContext } from '../../context/ChatContext';
 import { useFetchRecipientUser } from '../../hooks/useFetchRecipient';
@@ -13,7 +14,8 @@ const ChatBox = () => {
     loading: recipientLoading,
     error: recipientError,
   } = useFetchRecipientUser(currentChat);
-
+  const [textMessage , setTextMessage] = useState("")
+console.log("text", textMessage)
   useEffect(() => {
     // Perform any additional actions when currentChat or recipientUser changes
   }, [currentChat, recipientUser]);
@@ -54,6 +56,26 @@ const ChatBox = () => {
               </span>
             </Stack>
           ))}
+      </Stack>
+      <Stack direction="horizontal" gap={3} className="chat-input flex-grow-0">
+        <InputEmoji
+          value={textMessage}
+          onChange={setTextMessage}
+          fontFamily="nunito"
+          borderColor="rgba(72,112,223,0.2)"
+        />
+        <button className="send-btn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-send"
+            viewBox="0 0 16 16"
+          >
+            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
+          </svg>
+        </button>
       </Stack>
     </Stack>
   );
