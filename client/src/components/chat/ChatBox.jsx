8ec8ace24e9dd1d30257a4efcdbe02d680169df8
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-01-13 04:42:58 +0200

    docs chat box UI

--- client/src/components/chat/ChatBox.jsx
+++ client/src/components/chat/ChatBox.jsx
@@ -1,12 +1,13 @@
 // ChatBox.jsx
-
+import moment from 'moment';
 import { useContext, useEffect } from 'react';
 import { Stack } from 'react-bootstrap';
 import { ChatContext } from '../../context/ChatContext';
 import { useFetchRecipientUser } from '../../hooks/useFetchRecipient';
 
 const ChatBox = () => {
-  const { currentChat, isMessagesLoading } = useContext(ChatContext);
+  const { currentChat, isMessagesLoading, messages, user } =
+    useContext(ChatContext);
   const {
     recipientUser,
     loading: recipientLoading,
@@ -36,7 +37,24 @@ const ChatBox = () => {
       <div className="chat-header">
         <strong>{recipientUser?.name || 'No Name Available'}</strong>
       </div>
-      {/* Render your chat messages here using the 'messages' state */}
+      <Stack gap={3} className="messages">
+        {messages &&
+          messages.map((message, index) => (
+            <Stack
+              key={index}
+              className={`message ${
+                message?.senderId === user?._id
+                  ? 'self align-self-end flex-grow-0'
+                  : 'align-self-start flex-grow-0'
+              }`}
+            >
+              <span>{message.text}</span>
+              <span className="message-footer">
+                {moment(message.createdAt).calendar()}
+              </span>
+            </Stack>
+          ))}
+      </Stack>
     </Stack>
   );
 };
